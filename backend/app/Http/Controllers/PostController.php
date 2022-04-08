f0366e73a28325b56a53c0f434904b8d31fea85a
T-unity <takadanaritoshi@gmail.com>
2022-04-08 22:44:51 +0900

    不要なコメントアウトの削除

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Post;
-// use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
@@ -14,7 +13,6 @@ class PostController extends Controller
   {
 
     return view('/posts/index', [
-      // 'posts' => Post::all(),
       'posts' => Post::all()->sortByDesc('id')
     ]);
   }
@@ -26,18 +24,6 @@ class PostController extends Controller
 
   public function store( Request $req )
   {
-    // return;
-    // var_dump( $req );
-
-    // var_dump( $req->all() );
-    // var_dump( Auth::id() );
-
-    // dd( $req );
-    // var_dump( $req->get('content') );
-    // exit;
-
-    // $posted_user = Auth::id();
-
     // $post = Post::create([
     Post::create([
       'user_id' => Auth::id(),
@@ -45,7 +31,6 @@ class PostController extends Controller
       'content' => $req->get('content'),
     ]);
 
-    // return view( 'posts.index' );
     return redirect()->route('posts.index');
   }
 
