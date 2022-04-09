3c3718ca696e448f759f05853924446efbab87f6
T-unity <takadanaritoshi@gmail.com>
2022-04-09 16:12:30 +0900

    応募機能のためのルーティング設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,9 +13,15 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+// 投稿機能
 Route::get('/posts', 'App\Http\Controllers\PostController@index' )->name('posts.index');
 // Route::get('/posts', 'App\Http\Controllers\PostController@index' )->middleware(['auth'])->name('posts.index');
 Route::get('/posts/create', 'App\Http\Controllers\PostController@create' )->middleware(['auth']);
 // Route::post('/posts', 'App\Http\Controllers\PostController@index' );
 Route::post('/posts', 'App\Http\Controllers\PostController@store' )->middleware(['auth'])->name('posts.store');
 Route::get('/posts/{post}', 'App\Http\Controllers\PostController@show' )->name('posts.show');
+// 投稿機能
+
+// 応募機能
+Route::get('post/{id}/applicants', 'App\Http\Controllers\Applicant@store' )->name('post.applicants');
+// 応募機能
