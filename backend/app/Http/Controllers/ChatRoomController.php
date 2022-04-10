7e6eac2cba3109f2c8571490c352663509a565ae
T-unity <takadanaritoshi@gmail.com>
2022-04-10 19:34:03 +0900

    不要なコメントアウトの削除

--- backend/app/Http/Controllers/ChatRoomController.php
+++ backend/app/Http/Controllers/ChatRoomController.php
@@ -10,11 +10,6 @@ class ChatRoomController extends Controller
 {
   public function request($id)
   {
-    // リクエストを送信先のユーザーIDを特定
-    // リクエスト受信先のユーザーIDを特定
-    // chat_roomsのレコードを新規作成する。
-    // requested_user_idに送信ユーザー、received_user_idに受信先のユーザーID、room_statusに文字列でawaitを設定して処理完了
-
     $requested_user_id = (int)Auth::id();
     $received_user_id  = (int)$id;
 
