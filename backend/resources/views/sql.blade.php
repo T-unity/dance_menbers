41c1869c990e3202c47d9e4b4d1a974232765d48
T-unity <takadanaritoshi@gmail.com>
2022-04-15 22:11:44 +0900

    不要なコードの削除

--- backend/resources/views/sql.blade.php
+++ backend/resources/views/sql.blade.php
@@ -3,15 +3,8 @@
 <h1>SQLの実行</h1>
 
 <?php
-
-// $dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
-// $user = 'phper';
-// $pass = 'secret';
-// $pass = 'This is bad password';
-
 /**
  * PDOインスタンスを返却
- *
  * @return obj
  */
 function get_db_data(): PDO {
@@ -25,9 +18,6 @@ function get_db_data(): PDO {
 }
 
 try {
-  // var_dump( new Exception() );
-  // exit;
-  // $db = new PDO( $dsn, $user, $pass );
   get_db_data();
   echo 'succeed';
 } catch ( PDOException $e ) {
