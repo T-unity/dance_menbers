b7605253b8e1a99339654e0dd725705e91902599
T-unity <takadanaritoshi@gmail.com>
2022-04-15 21:09:50 +0900

    PDOを使用してMySQLとの疎通を確認

--- backend/resources/views/sql.blade.php
+++ backend/resources/views/sql.blade.php
@@ -1,10 +1,21 @@
 @extends('base')
 @section('content')
 <h1>SQLの実行</h1>
+
 <?php
 
+$dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
+$user = 'phper';
+$pass = 'secret';
 
-
+try {
+  $db = new PDO( $dsn, $user, $pass );
+  echo 'succeed';
+} catch ( PDOException $e ) {
+  die("error occured: {$e->getMessage()}");
+} finally {
+  $db = null;
+}
 
 ?>
 
