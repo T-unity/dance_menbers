96d311a0aeddae131c3e35daa1d32d3c2b6f4179
T-unity <takadanaritoshi@gmail.com>
2022-04-15 23:36:03 +0900

    コントローラ内でPDOを使用してinsertを実行

--- backend/app/Http/Controllers/SqlController.php
+++ backend/app/Http/Controllers/SqlController.php
@@ -3,11 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PDO;
+use PDOException;
 
 class SqlController extends Controller
 {
   public function some()
   {
     return view('sql');
+  }
+  public function insert()
+  {
+
+    /**
+     * PDOインスタンスを返却
+     * @return obj
+     */
+    function get_db_data(): PDO {
+      $dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
+      $user = 'phper';
+      $pass = 'secret';
+      // $pass = 'This is bad password';
+
+      $db = new PDO( $dsn, $user, $pass );
+      return $db;
+    }
+
+    try {
+      $db = get_db_data();
+
+      $que = 'INSERT INTO posts(title, content, user_id) VALUES(:title, :content, :user_id)';
+      $stt = $db->prepare($que);
+
+      $stt->bindValue(':title', $_POST['title']);
+      $stt->bindValue(':content', $_POST['content']);
+      $stt->bindValue(':user_id', $_POST['user_id']);
+
+      $stt->execute();
+
+      redirect()->route('sql');
+    } catch(PDOException $e) {
+      exit("error occured::: {$e->getMessage()}");
+    }
+
+    return redirect()->back();
   }
 }
--- backend/resources/views/sql.blade.php
+++ backend/resources/views/sql.blade.php
@@ -2,32 +2,6 @@
 @section('content')
 <h1>SQLの実行</h1>
 
-<?php
-/**
- * PDOインスタンスを返却
- * @return obj
- */
-function get_db_data(): PDO {
-  $dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
-  $user = 'phper';
-  $pass = 'secret';
-  // $pass = 'This is bad password';
-
-  $db = new PDO( $dsn, $user, $pass );
-  return $db;
-}
-
-// try {
-//   get_db_data();
-//   echo 'succeed';
-// } catch ( PDOException $e ) {
-//   exit("error occured: {$e->getMessage()}");
-// } finally {
-//   $db = null;
-// }
-
-?>
-
 <form method="POST" action="sql">
   @csrf
   <input name="title" type="text" placeholder="タイトル">
@@ -36,24 +10,5 @@ function get_db_data(): PDO {
 
   <button type="submit">登録</button>
 </form>
-
-<?php
-if (isset($_POST) & !empty($_POST)) {
-  try {
-    $db = get_db_data();
-
-    $que = 'INSERT INTO posts(title, content, user_id) VALUES(:title, :content, :user_id)';
-    $stt = $db->prepare($que);
-
-    $stt->bindValue(':title', $_POST['title']);
-    $stt->bindValue(':content', $_POST['content']);
-    $stt->bindValue(':user_id', $_POST['user_id']);
-
-    $stt->execute();
-  } catch(PDOException $e) {
-    exit("error occured::: {$e->getMessage()}");
-  }
-}
-?>
 
 @endsection
