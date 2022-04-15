5854e624cb7e218992a5630aede016300cdc97cd
T-unity <takadanaritoshi@gmail.com>
2022-04-15 21:29:57 +0900

    PDOインスタンスの生成を関数化

--- backend/resources/views/sql.blade.php
+++ backend/resources/views/sql.blade.php
@@ -4,12 +4,31 @@
 
 <?php
 
-$dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
-$user = 'phper';
-$pass = 'secret';
+// $dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
+// $user = 'phper';
+// $pass = 'secret';
+// $pass = 'This is bad password';
+
+/**
+ * PDOインスタンスを返却
+ *
+ * @return obj
+ */
+function get_db_data(): PDO {
+  $dsn = 'mysql:dbname=laravel_local; host=db; charaset=utf8';
+  $user = 'phper';
+  $pass = 'secret';
+  // $pass = 'This is bad password';
+
+  $db = new PDO( $dsn, $user, $pass );
+  return $db;
+}
 
 try {
-  $db = new PDO( $dsn, $user, $pass );
+  // var_dump( new Exception() );
+  // exit;
+  // $db = new PDO( $dsn, $user, $pass );
+  get_db_data();
   echo 'succeed';
 } catch ( PDOException $e ) {
   die("error occured: {$e->getMessage()}");
