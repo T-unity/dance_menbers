9e2fdaf4480a314fa17a1e83bb5255176d30d6e0
T-unity <takadanaritoshi@gmail.com>
2022-04-16 23:49:21 +0900

    seederの実行時間を計測する処理を追加

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,10 @@ class DatabaseSeeder extends Seeder
       //   'password' => Hash::make('password'),
       // ]);
 
+      $start = hrtime(true); // 計測開始時間
+
+      // 100件
+      User::factory()->count(100)->create();
       // 1,000件
       // User::factory()->count(1000)->create();
       // 10,000件
@@ -38,5 +42,19 @@ class DatabaseSeeder extends Seeder
       // User::factory()->count(100000000)->create();
       // 1,000,000,000件
       // User::factory()->count(1000000000)->create();
+
+      $end = hrtime(true);
+
+      $nano_sec = $end - $start;
+      $micro_sec = $nano_sec / 1000;
+      $milli_sec = $micro_sec / 1000;
+      $sec = $milli_sec / 1000;
+      echo '処理時間:'. $nano_sec .'ナノ秒' . PHP_EOL;
+      echo '処理時間:'. $micro_sec .'マイクロ秒' . PHP_EOL;
+      echo '処理時間:'. $milli_sec .'ミリ秒' . PHP_EOL;
+      echo '処理時間:'. $sec .'秒' . PHP_EOL;
+
+      exit;
+
     }
 }
