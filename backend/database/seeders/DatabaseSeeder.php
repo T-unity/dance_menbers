7ab64777e2e41b6442c078a614cb84225a28e3de
T-unity <takadanaritoshi@gmail.com>
2022-04-16 18:40:24 +0900

    ファクトリを使用してシードデータの作成

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,10 +18,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       // \App\Models\User::factory(10)->create();
-      DB::table('users')->insert([
-        'name' => Str::random(10),
-        'email' => Str::random(10).'@example.com',
-        'password' => Hash::make('password'),
-      ]);
+      // DB::table('users')->insert([
+      //   'name' => Str::random(10),
+      //   'email' => Str::random(10).'@example.com',
+      //   'password' => Hash::make('password'),
+      // ]);
+
+      User::factory()->count(1000)->create();
     }
 }
