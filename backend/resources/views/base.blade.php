7c8ed1f19c990f3b5b20b90ff3ddcf8ceec15c56
T-unity <takadanaritoshi@gmail.com>
2022-04-21 22:10:56 +0900

    グローバルナビの設定

--- backend/resources/views/base.blade.php
+++ backend/resources/views/base.blade.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- CSS -->
-  <link rel="stylesheet" href="{{ asset('static/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('/static/style.css') }}">
 
   <title>Document</title>
   <style>
@@ -21,16 +21,28 @@ use Illuminate\Support\Facades\Auth;
 </head>
 <body>
   <?= Auth::id() ?>
-  <br>
-  <a href="{{ route('dashboard') }}">ダッシュボード</a>
-  <br>
-  <a href="{{ route('top') }}">トップページ</a>
-  <br>
-  <a href="{{ route('posts.index') }}">投稿一覧</a>
-  <br>
-  <a href="{{ route('posts.create') }}">投稿の新規作成</a>
-  <br>
-  <a href="{{ route('sql') }}">投稿の新規作成 / PDO使用</a>
+
+  <header>
+    <div class='globalNav'>
+      <div class='inner'>
+        <div class='contents__wrap'>
+          <h1>
+            <a href='/'>Dancers</a>
+          </h1>
+          <div class='contents__menu'>
+            <ul class='contents__lists'>
+              <li><a href="{{ route('dashboard') }}">ダッシュボード</a></li>
+              <li><a href="{{ route('top') }}">トップページ</a></li>
+              <li><a href="{{ route('posts.index') }}">投稿一覧</a></li>
+              <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
+              <li><a href="{{ route('sql') }}">新規投稿（PDO）</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
   @yield('content')
 </body>
 </html>
