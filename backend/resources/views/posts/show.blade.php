18f919e8333412c40a08ef9ac06e1f9e11ee5458
T-unity <takadanaritoshi@gmail.com>
2022-04-21 22:22:54 +0900

    投稿詳細ページのデザイン微調整

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -7,27 +7,33 @@
   {{ session('successMessage') }}
 @endif
 
-<p>タイトル：<?= $post->title; ?></a></p>
-<p>本文：<?= $post->content; ?></a></p>
+<p>タイトル：<?= $post->title; ?></p>
+<p>本文：<?= $post->content; ?></p>
 <p>投稿者：<a href="<?= route('user.show', ['id' => $user->id]) ?>"><?= $user->name; ?></a></p>
 
 <br>
 
 <?php
 use App\Models\Applicant as ModelsApplicant;
-    if (ModelsApplicant::is_applied($post->id) === false
-      && ModelsApplicant::is_owned($post->id) === false): ?>
-  <a href="{{ route('post.applicants', ['id' => $post->id]) }}">応募する</a>
-<?php elseif (ModelsApplicant::is_owned($post->id)):
-//自分の投稿の場合は何も表示しない
-  else: ?>
+
+if (ModelsApplicant::is_applied($post->id) === false
+  && ModelsApplicant::is_owned($post->id) === false):
+  ?>
+  <div class="applicant">
+    <a href="{{ route('post.applicants', ['id' => $post->id]) }}">応募する</a>
+  </div>
+<?php
+  elseif (ModelsApplicant::is_owned($post->id)):
+    //自分の投稿の場合は何も表示しない
+  else:
+  ?>
   <p>この投稿に応募しました</p>
 <?php endif; ?>
 
 @endsection
 
 <style>
-  a {
+  .applicant a {
   border: solid 1px;
   border-radius: 3px;
   padding: 2px 1%;
