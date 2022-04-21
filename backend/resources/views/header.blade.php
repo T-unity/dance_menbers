6f422aef5914a8b5bdc2f965e783bdd599f6539e
T-unity <takadanaritoshi@gmail.com>
2022-04-22 00:19:12 +0900

    レイアウト調整

--- backend/resources/views/header.blade.php
+++ backend/resources/views/header.blade.php
@@ -11,11 +11,9 @@ use Illuminate\Support\Facades\Auth;
         </h1>
         <div class='contents__menu'>
           <ul class='contents__lists'>
-            <li><a href="{{ route('dashboard') }}">ダッシュボード</a></li>
             <li><a href="{{ route('top') }}">トップページ</a></li>
             <li><a href="{{ route('posts.index') }}">投稿一覧</a></li>
             <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
-            <li><a href="{{ route('sql') }}">新規投稿（PDO）</a></li>
             <li>
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
@@ -27,7 +25,7 @@ use Illuminate\Support\Facades\Auth;
                 </a>
               </form>
             </li>
-            <li>Sign in as <?= Auth::user()->name; ?></li>
+            <li><a href="{{ route('dashboard') }}"><?= Auth::user()->name; ?></a></li>
           </ul>
         </div>
       </div>
