1fb733d7f021d72755277d9e8068ba0d8d0d1013
T-unity <takadanaritoshi@gmail.com>
2022-04-22 23:16:45 +0900

    スタイル調整

--- backend/resources/views/posts/create.blade.php
+++ backend/resources/views/posts/create.blade.php
@@ -9,17 +9,24 @@
   <textarea id="post_create_form" class="form__post-content" cols="40" rows="8" name="content" type="text" placeholder="本文を入力"></textarea>
 
   <div class="form__post-btn__wrapper">
-    <button class="form__post-submit" type="submit">送信する</button>
+    <!-- <button class="form__post-submit" type="submit">送信する</button> -->
+    <span class="form__post-submit">投稿内容を確認</span>
   </div>
-
 </form>
 
+<div id="conf-modal" class="conf-post__wrap">
+  <h2>以下の内容で投稿してよろしいですか？</h2>
+</div>
 
 <script>
   const target = document.getElementById('post_create_form')
-  console.log(target)
-</script>
+  const modal = document.getElementById('conf-modal')
 
+  console.log(modal)
+  target.addEventListener('click', function() {
+    modal.style.display = 'block'
+  })
+</script>
 
 @endsection
 
@@ -41,7 +48,7 @@
     font-size: 14px;
   }
   .form__post-btn__wrapper {
-    padding-top: 3%;
+    padding-top: 5%;
     text-align: center;
   }
   .form__post-submit {
@@ -49,9 +56,16 @@
     background-color: rgba(105, 162, 255, .8);
     border-radius: 5px;
     border: none;
+    padding: 15px 30px;
     width: 180px;
     height: 55px;
     font-size: 19px;
     font-weight: 600;
+    cursor: pointer;
+  }
+
+  /* confirm modal */
+  .conf-post__wrap {
+    display: none;
   }
 </style>
